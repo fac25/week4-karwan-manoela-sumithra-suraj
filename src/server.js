e90e875e3ce32fdd6388ba4e7647f9e85b143a34
Suraj Pun <s_pun@outlook.com>
2022-10-11 16:09:08 +0100

    Added index & server structures

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const server = express();
+
+const cookieParser = require("cookie-parser");
+
+const staticHandler = express.static("public");
+
+const body = express.urlencoded({ extended: false });
+const cookies = cookieParser(process.env.COOKIE_SECERT);
+
+// Routes
+const home = require("./routes/home");
+const login = require("./routes/login");
+const signup = require("./routes/sign-up");
+const logout = require("./routes/log-out");
+const myhowdies = require("./routes/my-howdies");
+const deleteitem = require("./routes/delete");
+
+// Middlewear
+server.use(cookies);
+server.use(staticHandler);
+
+// Routes
+// server.get("/", home.get);
+
+// Export
+module.exports = server;

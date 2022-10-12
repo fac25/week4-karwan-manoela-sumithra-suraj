c3d9d728b126e8deb3ac6c02d1a8c69594d4ae18
Suraj Pun <s_pun@outlook.com>
2022-10-12 14:13:45 +0100

    Added Logout Function Relates#16

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const staticHandler = express.static("public");
 const body = express.urlencoded({ extended: false });
 const cookies = cookieParser(process.env.COOKIE_SECRET);
 
-
 // Routes
 const home = require("./routes/home");
 const login = require("./routes/login");
@@ -20,15 +19,18 @@ const deleteitem = require("./routes/delete");
 // Middlewear
 server.use(cookies);
 server.use(staticHandler);
-server.use(body)
+server.use(body);
 
 // Routes
 server.get("/", home.get);
 server.get("/sign-up", signup.get);
-server.post("/sign-up", signup.post)
-server.get("/log-in", login.get);
-server.post("/log-in", body, login.post)
+server.post("/sign-up", signup.post);
 
+server.get("/log-in", login.get);
+server.post("/log-in", body, login.post);
+server.post("/log-out", logout.post);
+
+// server.get("/my-howdies/:user_id", myhowdies.get);
 
 // Export
 module.exports = server;

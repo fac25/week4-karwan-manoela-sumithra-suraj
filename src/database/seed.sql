9e5f28f7f6019c2258cc8d2c55f0b0d6f2709f92
Suraj Pun <s_pun@outlook.com>
2022-10-12 14:36:22 +0100

    Created mock data Relates#7

---
+++ src/database/seed.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+INSERT INTO users VALUES
+(1, 'Karwan', 'k@hotmail.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJA', DATE('now')),
+(2, 'Manoela', 'm@hotmail.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJB', DATE('now')),
+(3, 'Sumithra', 's@hotmail.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJC', DATE('now')),
+(4, 'Suraj', 'ss@hotmail.com', '$2a$12$A74tKw96m82AEZpJrIEQxecZGscKayJD', DATE('now'))
+ON CONFLICT DO NOTHING;
+
+INSERT INTO howdies VALUES 
+(1, 'How to fix bike', 'Go to Halfords', '../public/images/fixbike.jpg', 1, '2022-10-12'),
+(2, 'How to sew a button', 'Use needle & thread', '../public/images/sewbutton.jpg', 3, '2022-10-11')
+ON CONFLICT DO NOTHING;
+
+COMMIT;

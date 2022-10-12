6105e034496d330b92b0e191b15fa3a36ecb10c3
Manoela <mo.tasheva@gmail.com>
2022-10-12 16:45:24 +0100

    Upload img and store src into database Closes #29

--- src/model/my-howdies.js
+++ src/model/my-howdies.js
@@ -11,4 +11,19 @@ function displayMyHowdies(user_id) {
     return get_my_howdies.all(user_id);
 }
 
-module.exports = { displayMyHowdies }
+const insert_howdie = db.prepare(/*sql*/ `
+    INSERT into howdies (title, content, image_src, user_id)
+    VALUES (
+        $title,
+        $content,
+        $img_src,
+        $user_id
+    )
+    RETURNING created_at
+`)
+
+function insertHowdie(title, content, img_src, user_id) {
+    return insert_howdie.get({title, content, img_src, user_id});
+};
+
+module.exports = { displayMyHowdies, insertHowdie }
--- src/routes/my-howdies.js
+++ src/routes/my-howdies.js
@@ -1,4 +1,5 @@
 const {myHowdiesHtml} = require('../templates')
+const { insertHowdie } = require("../model/my-howdies")
 
 function get(req, res){
     console.log(req.params.id)
@@ -6,6 +7,20 @@ function get(req, res){
     res.send(body)
 }
 
+function post(req, res) {
+    const image = req.file
+    // const img_path = "../images/" + image.path
+    const img_path = image.path.replace("public", "..")
+    const {title, content, } = req.body
 
+    // TO DO: get user_id from session
+    const user_id = req.params.id
+    console.log(req.body)
 
-module.exports = {get}
+    // req.file.mimetype .jpg
+    // get user_id
+    const date = insertHowdie(title, content, img_path, user_id).created_at;
+    res.redirect(`/my-howdies/${user_id}`)
+}
+
+module.exports = {get, post}
